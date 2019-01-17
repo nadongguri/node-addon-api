eca777edc93eacf12244190f5dd1fda2a3c46e2d
Dongjin Na <dongjinna@gmail.com>
2019-01-17 10:15:53 +0900

    Update cleanup.js

--- test/cleanup.js
+++ test/cleanup.js
@@ -1,44 +1,34 @@
-//'use strict';
-console.log('+____________+');
+'use strict';
 const buildType = process.config.target_defaults.default_configuration;
-const nodeMajorVersion = process.versions.node.split('.')[0];
-const nodeMinorVersion = process.versions.node.split('.')[1];
-const child_process = require('child_process');                       
+const assert = require('assert');
+const child_process = require('child_process');
+const bindingNode = `./build/${buildType}/binding.node`;
+const bindingNoexceptNode = `./build/${buildType}/binding_noexcept.node`;
 
+// we only check cleanup on 10.2 an higher were
 function checkNodeVersion() {
+  const nodeMajorVersion = process.versions.node.split('.')[0];
+  const nodeMinorVersion = process.versions.node.split('.')[1];
   if (nodeMajorVersion >= 10 && nodeMinorVersion >= 2)
     return true;
   else
     return false;
 }
 
-
-if (process.argv[2] === 'child') {                                    
-  console.log('child..........');
-}
-
-test(require(`./build/${buildType}/binding.node`), '1');
-test(require(`./build/${buildType}/binding_noexcept.node`), '2');
-
-function test(binding, name) {
+function test(bindingType) {
   if (!checkNodeVersion())
     return;
-  console.log(name);
-  console.log(process.execPath);
-  console.log(__filename);
-  //const { stdout } =
-          child_process.spawnSync(process.execPath, [__filename, 'child']);
-  //binding.cleanup.doCleanup();
-  /*
-  const common = require('../../common');
-  const assert = require('assert');                                
-                                                                        
-  if (process.argv[2] === 'child') {                                    
-	    require(`./build/${common.buildType}/binding`);                     
-  } else {                                                              
-	    const { stdout } =                                                  
-			    child_process.spawnSync(process.execPath, [__filename, 'child']); 
-		  assert.strictEqual(stdout.toString().trim(), 'cleanup(42)');        
-  }                                                                     
-  */
+  const { stdout } =
+          child_process.spawnSync(process.execPath, [__filename, bindingType]);
+  assert.strictEqual(stdout.toString().trim(), 'cleanup(42)');
+}
+
+if (process.argv[2] === bindingNode ||
+    process.argv[2] === bindingNoexceptNode) {
+  const binding = require(process.argv[2]);
+  binding.cleanup.doCleanup();
+}
+else {
+  test(bindingNode);
+  test(bindingNoexceptNode);
 }
